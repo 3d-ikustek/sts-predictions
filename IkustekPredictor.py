7e8a1cf390dd2ffac2d185fa9c8d9e5bca4fc62e
Aritz Galdos Otermin <aritz@indaba.es>
2019-04-24 12:57:38 +0200

    prompt

--- IkustekPredictor.py
+++ IkustekPredictor.py
@@ -1,4 +1,17 @@
 from dataManager.RainDataManager import RainDataManager
 
+futureStepsNum = 7
+
+futureTemp = []
+
 rainDM = RainDataManager()
-rainDM.workData()
+# rainDM.workData()
+
+
+for i in range(0, futureStepsNum):
+    promptStr = "Please enter temp for day +" + str(i) + ": "
+    temp = float(input(promptStr))
+    print("You entered: " + str(temp))
+    futureTemp.append(temp)
+
+print(futureTemp)
